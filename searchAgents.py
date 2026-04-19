16357a27c2adc8c13da9eb64930ec144cfb0e049
Alec Situ <92289021+Err0r64@users.noreply.github.com>
2026-04-19 02:00:00 -0700

    Alec - completed closest dot

--- searchAgents.py
+++ searchAgents.py
@@ -618,8 +618,9 @@ class ClosestDotSearchAgent(SearchAgent):
         walls = gameState.getWalls()
         problem = AnyFoodSearchProblem(gameState)
 
-        "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        # To find the closest dot, we will just run BFS on the AnyFoodSearchProblem
+        # works because we explored the closest layer first.
+        return search.bfs(problem)
 
 
 class AnyFoodSearchProblem(PositionSearchProblem):
@@ -656,7 +657,7 @@ class AnyFoodSearchProblem(PositionSearchProblem):
         x, y = state
 
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        return self.food[x][y]
 
 
 def mazeDistance(point1, point2, gameState):
